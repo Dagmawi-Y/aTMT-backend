86fdc58cbd2c35e5dc2b2cf6b1c31ff69e5b5a80
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-09-18 02:25:03 +0300

    some fixes

--- apps/content-generation/src/main.ts
+++ apps/content-generation/src/main.ts
@@ -3,19 +3,14 @@ import { BlogGeneratorModule } from './blog-generator/blog-generator.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  // Create a hybrid application
   const app = await NestFactory.create(BlogGeneratorModule);
 
-  // Configure the RabbitMQ microservice
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.RMQ,
-    // Add your RabbitMQ options here
   });
 
-  // Start listening for microservice messages
   await app.startAllMicroservices();
 
-  // Start listening for HTTP requests on port 3333
   await app.listen(3333);
 
   console.log(`Application is running on: ${await app.getUrl()}`);
